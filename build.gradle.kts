8c7b7127f2ee86b9af6b95c950d599f12a0f6bfa
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2024-11-02 11:38:50 +0800

    Update to Cosmic Reach 0.3.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ object Constants {
 
     const val DISPLAY_NAME = "Simply Cosmic Shading"
 
-    const val VERSION_COSMIC_REACH = "0.3.3"
+    const val VERSION_COSMIC_REACH = "0.3.4"
     const val VERSION_JAVA = "17"
 }
 
